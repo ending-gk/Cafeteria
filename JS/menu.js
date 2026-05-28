3317b80ba0a769be27fec949f027d79dc33c4306
ending-gk <davi.passos@aluno.ifsp.edu.br>
2026-05-28 18:38:51 -0300

    termino-sidebar

---
+++ JS/menu.js
@@ -0,0 +1,5 @@
+const exp = document.getElementById("menu-id");
+const navm = document.getElementById("nav-menu");
+exp.addEventListener('click',function(){
+    navm.classList.toggle('expandir')
+})
