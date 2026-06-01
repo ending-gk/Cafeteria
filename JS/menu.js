6192cfe15856fd718341689f040a4b76ef2a2bde
ending-gk <davi.passos@aluno.ifsp.edu.br>
2026-06-01 18:36:37 -0300

    resolver o menu lateral :)

--- JS/menu.js
+++ JS/menu.js
@@ -1,5 +1,12 @@
 const exp = document.getElementById("menu-id");
 const navm = document.getElementById("nav-menu");
-exp.addEventListener('click',function(){
+const fc = document.getElementById("fc");
+exp.addEventListener('click',()=>{
     navm.classList.toggle('expandir')
+    if(navm.classList.contains("expandir")){
+        fc.style.display="block";
+    }
+    else{
+        fc.style.display="none";
+    }
 })
